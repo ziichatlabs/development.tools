862f5952f68f6dc3ffbf527864eb2864991330f3
Huy Trần <tranlamhuy5tn@gmail.com>
2024-08-19 09:18:32 +0700

    release: 0.1.5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4108167003"
+    serviceWorkerVersion: "1507970015"
   }
 });
